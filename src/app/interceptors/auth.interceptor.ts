ae48121a91587534893670d6cb372e8a8cca5ffc
Sajeed <mdsajeedmds@gmail.com>
2024-08-20 22:01:57 +0530

    403 redirection bug fix

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -9,6 +9,7 @@ import { inject } from '@angular/core';
 import { JwtService } from '../services/jwt/jwt.service';
 import { LoaderService } from '../services/loader/loader.service';
 import { catchError, finalize, map, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
@@ -16,10 +17,17 @@ export const authInterceptor: HttpInterceptorFn = (
 ) => {
   const token = inject(JwtService).getToken();
   const loaderService = inject(LoaderService);
+  const router=inject(Router);
   let modifiedReq = req.clone({
     headers: req.headers.append('Authorization', `Bearer ${token}`),
   });
 
   loaderService.startLoader();
-  return next(modifiedReq);
+  return next(modifiedReq).pipe(
+    catchError((error:HttpErrorResponse)=>{
+      if(error.status==403)
+        router.navigate(['login'])
+      return throwError(()=>error)
+    })
+  );
 };
